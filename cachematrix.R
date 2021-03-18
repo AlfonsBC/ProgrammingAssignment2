684a3c0bb1b3196a1663d693c5eb1f73b146f82e
AlfonsBC <albace2000@hotmail.com>
2021-03-17 19:20:32 -0600

    Adding R code

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,24 @@
 
 ## Write a short comment describing this function
 
+## makeCacheMatrix creates an object to hold a matrix
+## and (eventually) its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  #Initialize the inverse 
+  inverse_matrix <- NULL
+  
+  #Setting the matrix
+  set<-function(matrix){
+    m <<- matrix
+    inverse_matrix <<- NULL
+  }
+  
+  #Getting the matrix
+  get<-function() x
+  setinverse_m<-function(inverse) {inverse_matrix <<- inverse}
+  getinverse_m<-function() {inverse_matrix}
+  
+  list(set=set, get=get, setinverse_m=setinverse_m ,getinverse_m=getinverse_m)
 }
 
 
@@ -12,4 +28,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    cached <- x$getinverse_m()
+    
+    if(!is.null(cached)){
+      return(cached)
+    }
+    
+    m<- x$get()
+    cached <- solve(m)
+    x$setinverse_m(cached)
+    cached
 }
